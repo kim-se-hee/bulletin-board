8f1dd934de4ccf01b0384eb665775df291ec01db
kim-se-hee <saree98@naver.com>
2025-03-07 13:36:49 +0900

    test: 게시글 작성자로 검색 테스트

--- src/test/java/ksh/bulletinboard/domain/post/service/PostServiceTest.java
+++ src/test/java/ksh/bulletinboard/domain/post/service/PostServiceTest.java
@@ -2,6 +2,8 @@ package ksh.bulletinboard.domain.post.service;
 
 import ksh.bulletinboard.domain.board.domain.Board;
 import ksh.bulletinboard.domain.board.repository.BoardRepository;
+import ksh.bulletinboard.domain.member.domain.Member;
+import ksh.bulletinboard.domain.member.repository.MemberRepository;
 import ksh.bulletinboard.domain.post.domain.Post;
 import ksh.bulletinboard.domain.post.repository.PostRepository;
 import ksh.bulletinboard.domain.post.service.dto.response.PostPageResponse;
@@ -29,6 +31,9 @@ class PostServiceTest {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @DisplayName("요청한 게시판의 게시글 목록 페이지를 조회한다")
     @Test
@@ -107,10 +112,51 @@ class PostServiceTest {
                 .containsExactly(tuple("글456", 1L));
 
     }
-    
+
+    @DisplayName("요청한 게시판의 게시글을 작성자로 검색 후 게시글의 목록을 페이지로 조회한다")
+    @Test
+    void getPostsOfBoardAndMemberNickname() {
+        //given
+        Board board = createBoard();
+        boardRepository.save(board);
+
+        Member member1 = createMember("회원1");
+        Member member2 = createMember("회원2");
+        memberRepository.saveAll(List.of(member1, member2));
+
+        Post post1 = createPost("글123", board, member1);
+        Post post2 = createPost("글456", board, member1);
+        Post post3 = createPost("글789", board, member2);
+        postRepository.saveAll(List.of(post1, post2, post3));
+
+        PageRequest pageRequest = PageRequest.of(0, 2);
+
+        //when
+        PostPageResponse response = postService.getPostsOfBoardByNickname(board.getId(), "회원1", pageRequest);
+
+        //then
+        assertThat(response)
+                .extracting("pageNum", "pageSize", "totalPages", "hasNext")
+                .containsExactly(0, 2, 1, false);
+
+        assertThat(response.getPostResponses()).hasSize(2)
+                .extracting("title", "views")
+                .containsExactly(
+                        tuple("글123", 1L),
+                        tuple("글456", 1L)
+                );
+
+    }
+
     private static Board createBoard() {
         return Board.builder()
                 .title("자유게시판")
+                .build();
+    }
+
+    private static Member createMember(String member1) {
+        return Member.builder()
+                .nickname(member1)
                 .build();
     }
 
@@ -119,6 +165,15 @@ class PostServiceTest {
                 .title(title)
                 .views(1L)
                 .board(board)
+                .build();
+    }
+
+    private static Post createPost(String title, Board board, Member member) {
+        return Post.builder()
+                .title(title)
+                .views(1L)
+                .board(board)
+                .member(member)
                 .build();
     }
     
