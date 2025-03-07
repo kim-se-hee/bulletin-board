414971a2971d019f3c4a8146740a8597ae57bc81
kim-se-hee <saree98@naver.com>
2025-03-07 16:50:22 +0900

    test: 대댓글 조회 API 테스트

---
+++ src/test/java/ksh/bulletinboard/domain/reply/controller/ReplyControllerTest.java
@@ -0,0 +1,47 @@
+package ksh.bulletinboard.domain.reply.controller;
+
+import ksh.bulletinboard.domain.reply.service.ReplyService;
+import ksh.bulletinboard.domain.reply.service.dto.ReplyServiceResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ReplyController.class)
+class ReplyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ReplyService replyService;
+
+    @DisplayName("댓글의 대댓글을 조회한다")
+    @Test
+    void allReplies() throws Exception {
+        //given
+        List<ReplyServiceResponse> response = List.of();
+        given(replyService.getRepliesOfComment(anyLong()))
+                .willReturn(response);
+
+        //when //then
+        mockMvc.perform(
+                        get("/post/1/reply")
+                ).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.replies").isArray());
+
+    }
+
+}
