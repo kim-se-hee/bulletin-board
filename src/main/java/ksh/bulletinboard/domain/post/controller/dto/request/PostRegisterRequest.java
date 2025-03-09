9655794e86e3bd1fc945a73f3f85a23310cb8ea8
kim-se-hee <saree98@naver.com>
2025-03-09 19:29:40 +0900

    fix: 누락한 요청 DTO 검증 로직 추가

--- src/main/java/ksh/bulletinboard/domain/post/controller/dto/request/PostRegisterRequest.java
+++ src/main/java/ksh/bulletinboard/domain/post/controller/dto/request/PostRegisterRequest.java
@@ -1,5 +1,8 @@
 package ksh.bulletinboard.domain.post.controller.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import ksh.bulletinboard.domain.post.service.dto.request.PostRegisterServiceRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,13 +11,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PostRegisterRequest {
 
+    @NotBlank
     String title;
+
+    @NotBlank
     String content;
-    long memberId;
-    long boardId;
+
+    @NotNull
+    Long memberId;
+
+    @NotNull
+    Long boardId;
 
     public PostRegisterServiceRequest toServiceRequest() {
         return PostRegisterServiceRequest.of(title, content, memberId, boardId);
+    }
+
+    public PostRegisterRequest(String title, String content, Long memberId, Long boardId) {
+        this.title = title;
+        this.content = content;
+        this.memberId = memberId;
+        this.boardId = boardId;
     }
 
 }
