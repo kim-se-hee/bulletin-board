fb6e7115246bf734cabce1c40adc30c15f341222
kim-se-hee <saree98@naver.com>
2025-03-09 22:27:23 +0900

    feat: 자신이 작성한 대댓글 내용 수정 구현

--- src/main/java/ksh/bulletinboard/domain/reply/service/ReplyService.java
+++ src/main/java/ksh/bulletinboard/domain/reply/service/ReplyService.java
@@ -39,4 +39,13 @@ public class ReplyService {
         return ReplyServiceResponse.from(reply);
     }
 
+    @Transactional
+    public ReplyServiceResponse editReply(long id, String content){
+        Reply reply = replyRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 대댓글입니다"));
+
+        reply.editContent(content);
+        return ReplyServiceResponse.from(reply);
+    }
+
 }
