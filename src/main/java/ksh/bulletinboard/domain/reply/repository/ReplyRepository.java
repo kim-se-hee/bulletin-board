0d65bad6f34a02fd7043e430229f685c2d3e445f
kim-se-hee <saree98@naver.com>
2025-03-09 22:25:38 +0900

    fix: JpaRepository 기본키 타입 수정

--- src/main/java/ksh/bulletinboard/domain/reply/repository/ReplyRepository.java
+++ src/main/java/ksh/bulletinboard/domain/reply/repository/ReplyRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ReplyRepository extends JpaRepository<Reply, Integer> {
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     List<Reply> findByCommentId(long id);
 
