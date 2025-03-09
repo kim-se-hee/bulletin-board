b9f69190578e7299e4fc1990c037602cbbc77661
kim-se-hee <saree98@naver.com>
2025-03-09 20:33:27 +0900

    feat: 대댓글 작성 구현

--- src/main/java/ksh/bulletinboard/domain/reply/service/ReplyService.java
+++ src/main/java/ksh/bulletinboard/domain/reply/service/ReplyService.java
@@ -1,5 +1,8 @@
 package ksh.bulletinboard.domain.reply.service;
 
+import ksh.bulletinboard.domain.comment.domain.Comment;
+import ksh.bulletinboard.domain.comment.repository.CommentRepository;
+import ksh.bulletinboard.domain.reply.domain.Reply;
 import ksh.bulletinboard.domain.reply.service.dto.ReplyServiceResponse;
 import ksh.bulletinboard.domain.reply.repository.ReplyRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +17,26 @@ import java.util.List;
 public class ReplyService {
 
     private final ReplyRepository replyRepository;
+    private final CommentRepository commentRepository;
 
     public List<ReplyServiceResponse> getRepliesOfComment(long id){
         return replyRepository.findByCommentId(id).stream()
                 .map(ReplyServiceResponse::from)
                 .toList();
+    }
+
+    @Transactional
+    public ReplyServiceResponse createReply(String content, long commentId){
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다"));
+
+        Reply reply = Reply.builder()
+                .content(content)
+                .comment(comment)
+                .build();
+        replyRepository.save(reply);
+
+        return ReplyServiceResponse.from(reply);
     }
 
 }
