edc474f18f94fa90e55ae5a45696284bf5d22d22
kim-se-hee <saree98@naver.com>
2025-03-17 01:45:11 +0900

    refactor: 첨부 파일 관련 작업의 책임을  FileUtils로 이동 - 저장, 조회, 인코딩을 FileUtils에서 담당 - AttachmentService는 단순히 유틸 클래스의 메소드를 이용

--- src/main/java/ksh/bulletinboard/domain/attachment/service/AttachmentService.java
+++ src/main/java/ksh/bulletinboard/domain/attachment/service/AttachmentService.java
@@ -2,15 +2,15 @@ package ksh.bulletinboard.domain.attachment.service;
 
 import ksh.bulletinboard.domain.attachment.domain.Attachment;
 import ksh.bulletinboard.domain.attachment.repository.AttachmentRepository;
-import ksh.bulletinboard.domain.attachment.service.dto.response.AttachmentServiceResponse;
+import ksh.bulletinboard.domain.post.domain.Post;
 import ksh.bulletinboard.global.util.FileUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,18 +18,24 @@ public class AttachmentService {
 
     private final AttachmentRepository attachmentRepository;
 
-    public AttachmentServiceResponse getAttachment(long id) throws MalformedURLException {
-        Attachment attachment = attachmentRepository.findById(id)
+    public Attachment getAttachment(long id) throws MalformedURLException {
+        return attachmentRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 파일입니다"));
-
-        String fullPath = FileUtils.createFullPath(attachment.getStoredFilename());
-        UrlResource resource = new UrlResource("file:" + fullPath);
-
-        String encodedOriginalFileName = UriUtils.encode(attachment.getOriginalFilename(), StandardCharsets.UTF_8);
-
-        return new AttachmentServiceResponse(encodedOriginalFileName, resource);
-
     }
 
+    //TODO:글 등록 시 첨부파일 명이 보여야 하므로 생성한 첨부파일 리스트 반환하기
+    //TODO:forEach, stream을 통한 리팩토링
+    public void saveAttachments(List<MultipartFile> multipartFiles, Post post) throws IOException {
+        for (MultipartFile multipartFile : multipartFiles) {
+            String storedName = FileUtils.saveFile(multipartFile);
+
+            Attachment attachment = Attachment.builder()
+                    .originalFilename(multipartFile.getOriginalFilename())
+                    .storedFilename(storedName)
+                    .post(post)
+                    .build();
+            attachmentRepository.save(attachment);
+        }
+    }
 
 }
--- src/main/java/ksh/bulletinboard/global/util/FileUtils.java
+++ src/main/java/ksh/bulletinboard/global/util/FileUtils.java
@@ -1,20 +1,37 @@
 package ksh.bulletinboard.global.util;
 
+import org.springframework.core.io.UrlResource;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class FileUtils {
 
     private static String FILE_DIR = "C:\\Users\\saree98\\Desktop\\attachment\\";
 
-    public static String createStoredFileName(String originalName) {
+    public static String saveFile(MultipartFile multipartFile) throws IOException {
+        String originalName = multipartFile.getOriginalFilename();
         String extension = originalName.substring(originalName.lastIndexOf("."));
         UUID uuid = UUID.randomUUID();
+        String storedName = uuid + extension;
 
-        return uuid + extension;
+        String fullPath = FILE_DIR + storedName;
+        multipartFile.transferTo(new File(fullPath));
+
+        return storedName;
     }
 
-    public static String createFullPath(String fileName) {
-        return FILE_DIR + fileName;
+    public static UrlResource findStoredFileResource(String fileName) throws IOException {
+        String fullPath = FILE_DIR + fileName;
+        return new UrlResource("file:" + fullPath);
+    }
+
+    public static String encodeFileName(String fileName) {
+        return UriUtils.encode(fileName, StandardCharsets.UTF_8);
     }
 
 }
